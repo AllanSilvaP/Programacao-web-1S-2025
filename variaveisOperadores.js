d6982a17b7f98428f9867389cd163da9afcd3a9e
AllanSilvaP <Allan.dasilvap2005@gmail.com>
2025-03-07 09:17:47 -0300

    Tester

---
+++ variaveisOperadores.js
@@ -0,0 +1,7 @@
+let num1, num2, soma
+
+num1 = 10
+num2 = 15
+soma = num1 + num2
+
+console.log(`A soma do número ${num1} + ${num2} = ${soma}`)
---
+++ variaveis_tipo.js
@@ -0,0 +1,14 @@
+const n = 1
+
+console.log('valor de N = '+n)
+
+var j = 3
+j = 4
+
+console.log('j = '+j)
+
+let nome = "João"
+let telefone = '1234'
+
+console.log(`Meu nome é ${nome}, meu telefone é ${telefone}`)
+console.log('testando')
