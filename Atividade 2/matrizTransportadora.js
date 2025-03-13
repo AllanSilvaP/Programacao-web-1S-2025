1f716460728196aa6f82d840c50fd5d8a875dd51
Allan Da Silva <155674133+AllanSilvaP@users.noreply.github.com>
2025-03-13 10:08:46 -0300

    Add files via upload

---
+++ Atividade 2/matrizTransportadora.js
@@ -0,0 +1,11 @@
+function transporMatriz() {
+    var array = [[1,2], [3,4], [5,6]]
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            console.log (array[i][j])
+        }
+        
+    }
+}
+
+transporMatriz()
