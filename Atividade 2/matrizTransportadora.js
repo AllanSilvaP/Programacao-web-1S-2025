066d2a62d80ffbd5c216752719a6fe1d1e51731b
Allan Da Silva <155674133+AllanSilvaP@users.noreply.github.com>
2025-03-14 09:27:31 -0300

    Update matrizTransportadora.js

--- Atividade 2/matrizTransportadora.js
+++ Atividade 2/matrizTransportadora.js
@@ -1,11 +1,28 @@
-function transporMatriz() {
-    var array = [[1,2], [3,4], [5,6]]
+var array = [[1,2], [3,4], [5,6]]
+
+function transporMatriz(array) {
+    console.log('Matriz\n')
+
     for (let i = 0; i < array.length; i++) {
+        let linha = ''
         for (let j = 0; j < array[i].length; j++) {
-            console.log (array[i][j])
+            linha += array[i][j] + "\t"
         }
-        
+        console.log (linha)
+    }
+    console.log('')
+
+    //transpor matriz
+
+    console.log("============= MATRIZ TRANSPOSTA =============", end="\n")
+
+    for (let j = 0; j < array[j].length; j++) {
+        let linha = ''
+        for (let i = 0; i < array.length; i++) {
+            linha += array[i][j] + "\t"
+        }
+        console.log (linha)
     }
 }
 
-transporMatriz()
+transporMatriz(array)
