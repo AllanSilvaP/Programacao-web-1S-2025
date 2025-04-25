a4d38f573a741e625baa4a33570f4db7519c5e70
AllanSilvaP <Allan.silvap2005@gmail.com>
2025-04-25 09:35:59 -0300

    Formulário

---
+++ Atividade5/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(8080, () => {
+    console.log('Servidor rodando na porta 8080')
+})
